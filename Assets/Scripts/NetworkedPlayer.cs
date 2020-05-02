0928b413467be8ad1418db6637f4d24cf613feb2
Aria Kraft <kraft.aria@gmail.com>
2020-05-02 10:18:00 -0700

    Added network test scene

---
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayer : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public GameObject avatar;
+    public Transform playerGlobal;
+    public Transform playerLocal;
+
+    private void Start()
+    {
+        Debug.Log("NetworkedPlayer Start");
+
+        if (photonView.IsMine)
+        {
+            playerGlobal = GameObject.Find("OVRPlayerController").transform;
+            playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+
+            transform.SetParent(playerLocal);
+            transform.localPosition = Vector3.zero;
+
+            avatar.SetActive(false);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(playerGlobal.position);
+            stream.SendNext(playerGlobal.rotation);
+            stream.SendNext(playerLocal.position);
+            stream.SendNext(playerLocal.rotation);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+            avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+            avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
