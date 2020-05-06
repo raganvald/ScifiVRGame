b45a5aa6e6bca2972c28353d009d2d4459303775
GuchBot <guchfunmail@gmail.com>
2020-05-05 23:00:58 -0700

    new file:   Assets/PREFABS.meta 	new file:   Assets/PREFABS/Enemy.prefab 	new file:   Assets/PREFABS/Enemy.prefab.meta 	new file:   Assets/Scenes/DemoSceneOVR.meta 	new file:   Assets/Scenes/DemoSceneOVR/NavMesh.asset 	new file:   Assets/Scenes/DemoSceneOVR/NavMesh.asset.meta 	new file:   Assets/Scripts/Controllers.meta 	new file:   Assets/Scripts/Controllers/EnemyController.cs 	new file:   Assets/Scripts/Controllers/EnemyController.cs.meta 	new file:   Assets/Scripts/PlayerMgr.cs 	new file:   Assets/Scripts/PlayerMgr.cs.meta 	new file:   ScifiVRGame

---
+++ Assets/Photon/Scripts/NetworkedPlayerOI.cs
@@ -0,0 +1,55 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayerOI : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public GameObject networkPlayerHead, networkPlayerLHand, networkPlayerRHand;
+    Transform playerHead, playerLHand, playerRHand;
+
+    private void Start()
+    {
+        if (photonView.IsMine)
+        {
+            playerHead = GameObject.Find("CenterEyeAnchor").transform;
+            playerLHand = GameObject.Find("LeftControllerAnchor").transform;
+            playerRHand = GameObject.Find("RightControllerAnchor").transform;
+
+            networkPlayerHead.transform.SetParent(playerHead);
+            networkPlayerLHand.transform.SetParent(playerLHand);
+            networkPlayerRHand.transform.SetParent(playerRHand);
+
+            networkPlayerHead.SetActive(false);
+            networkPlayerLHand.SetActive(false);
+            networkPlayerRHand.SetActive(false);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(playerHead.position);
+            stream.SendNext(playerHead.rotation);
+
+            stream.SendNext(playerLHand.position);
+            stream.SendNext(playerLHand.rotation);
+
+            stream.SendNext(playerRHand.position);
+            stream.SendNext(playerRHand.rotation);
+        }
+        else
+        {
+            networkPlayerHead.transform.position = (Vector3)stream.ReceiveNext();
+            networkPlayerHead.transform.rotation = (Quaternion)stream.ReceiveNext();
+
+            networkPlayerLHand.transform.position = (Vector3)stream.ReceiveNext();
+            networkPlayerLHand.transform.rotation = (Quaternion)stream.ReceiveNext();
+
+            networkPlayerRHand.transform.position = (Vector3)stream.ReceiveNext();
+            networkPlayerRHand.transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMgr : MonoBehaviour{
+
+    #region Singleton
+
+    public static PlayerMgr instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+
+    public GameObject player;
+
+}
