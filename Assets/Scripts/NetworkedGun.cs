93d7e026fb1b4f6731806293497d7ee34e2be6f3
Aria Kraft <kraft.aria@gmail.com>
2020-05-07 11:39:15 -0700

    added gun sync - with ownership transfer

---
+++ Assets/Scripts/NetworkedGun.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class NetworkedGun : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (photonView.Owner != PhotonNetwork.LocalPlayer)
+            photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
+    }
+}
