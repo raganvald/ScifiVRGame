d921e317e60e1818bc5f1340fa7f22d4252bc592
Aria Kraft <kraft.aria@gmail.com>
2020-05-07 12:03:05 -0700

    added enemy network sync

---
+++ Assets/Scripts/NetworkEnemySpawnPoint.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkEnemySpawnPoint : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+    public float spawnRate;
+    public int batchSpawnCount;
+    public float batchRate;
+    public string ObjectToSpawnStr;
+
+
+    public void Awake()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(batchRate);
+            for (int i=0; i < batchSpawnCount; i++)
+            { 
+                yield return new WaitForSeconds(spawnRate);
+                GameObject go = PhotonNetwork.Instantiate(ObjectToSpawnStr, this.transform.position, Quaternion.identity, 0);
+                go.transform.parent = null;
+            }
+        }
+
+    }
+}
