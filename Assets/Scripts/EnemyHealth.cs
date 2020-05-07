7a2007b1da71d3ad1b6a58a372f030296ac445b9
Raganvald <raganvald@gmail.com>
2020-05-06 19:34:39 -0700

    Added zombie animations, and first impression

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHp = 10;
+    private int hp;
+    void SetKinematic(bool newValue)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.isKinematic = newValue;
+        }
+    }
+    void Start()
+    {
+        SetKinematic(true);
+        hp = maxHp;
+    }
+    public void Damage(int damage)
+    {
+        if (hp <= 0) return;
+        hp -= damage;
+        if (hp <= 0) Die();
+    }
+    void Die()
+    {
+        SetKinematic(false);
+        GetComponent<Animator>().enabled = false;
+        Destroy(gameObject, 5);
+    }
+}
