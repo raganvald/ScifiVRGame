049e3fcb7ae95a6d2dccc0e242019955d44aab15
Aria Kraft <kraft.aria@gmail.com>
2020-05-07 21:44:26 -0700

    added pause to restart menu and cleaned up restart menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    GameObject restartMenuCanvas;
+    void Start()
+    {
+        restartMenuCanvas = transform.Find("RestartMenuCanvas").gameObject;
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            restartMenuCanvas.SetActive(!restartMenuCanvas.activeSelf);
+            Time.timeScale = restartMenuCanvas.activeSelf ? 0 : 1;
+        }
+    }
+
+    public void DoRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void DoQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif   
+    }
+}
