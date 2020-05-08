056c3783cf87b85b6e2b14694a7fb6cbbcee4ae4
Aria Kraft <kraft.aria@gmail.com>
2020-05-07 20:57:10 -0700

    added MenuTest for restart menu

---
+++ Assets/RestartMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartMenu : MonoBehaviour
+{
+    public void DoRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
