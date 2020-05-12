4f03a4555779cacb9d5cdd8be6f22b1d64f8d3af
Raganvald <raganvald@gmail.com>
2020-05-12 00:25:28 -0700

    -Upgraded to unity 2019.3.1 -Created IK body

---
+++ Assets/Scripts/VRAnimatorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRAnimatorController : MonoBehaviour
+{
+    [Range(0,1)]
+    public float smoothing = 1f;
+    private Animator animator;
+    private Vector3 previousPos;
+    private VRIKRig vrRig;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        vrRig = GetComponent<VRIKRig>();
+        previousPos = vrRig.head.vrTarget.position;
+    }
+
+    void Update()
+    {
+
+        Vector3 headsetSpead = (vrRig.head.vrTarget.position - previousPos) / Time.deltaTime;
+        headsetSpead.y = 0;
+
+        //Local speed
+        Vector3 headsetLocalSpeed = transform.InverseTransformDirection(headsetSpead);
+        previousPos = vrRig.head.vrTarget.position;
+
+
+        //Set Animator Values
+        float previousDirectionX = animator.GetFloat("DirectionX");
+        float previousDirectionY = animator.GetFloat("DirectionY");
+
+        animator.SetFloat("DirectionX", Mathf.Lerp(previousDirectionX, Mathf.Clamp(headsetLocalSpeed.x, -1, 1), smoothing));
+        animator.SetFloat("DirectionY", Mathf.Lerp(previousDirectionX, Mathf.Clamp(headsetLocalSpeed.z, -1, 1), smoothing));
+
+    }
+}
---
+++ Assets/Scripts/VRFootIK.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRFootIK : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float rightFootPoseWeight = 1;
+    [Range(0, 1)]
+    public float leftFootPoseWeight = 1;
+
+    [Range(0, 1)]
+    public float rightFootRotWeight = 1;
+    [Range(0, 1)]
+    public float leftFootRotWeight = 1;
+
+    public Vector3 footOffset;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnAnimatorIK(int layerIndex)
+    {
+        //Ignore hits on the player layer 
+        int layerMask = 1 << 10;
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+
+        //Handle the Right foot
+        Vector3 rightFootPos = animator.GetIKPosition(AvatarIKGoal.RightFoot);
+        bool hasHit = Physics.Raycast(rightFootPos + Vector3.up, Vector3.down, out hit, layerMask);
+        if (hasHit)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, rightFootPoseWeight);
+            animator.SetIKPosition(AvatarIKGoal.RightFoot, hit.point + footOffset);
+
+            //Fix rotation of foot with offset
+            Quaternion rightFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, rightFootRotWeight);
+            animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootRotation);
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+        }
+
+        //Handle the Left foot
+        Vector3 leftFootPos = animator.GetIKPosition(AvatarIKGoal.LeftFoot);
+        hasHit = Physics.Raycast(leftFootPos + Vector3.up, Vector3.down, out hit, layerMask);
+        if (hasHit)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, leftFootPoseWeight);
+            animator.SetIKPosition(AvatarIKGoal.LeftFoot, hit.point + footOffset);
+            Quaternion footRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
+            
+            //Fix rotation of foot with offset
+            Quaternion leftFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, leftFootRotWeight);
+            animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootRotation);
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+        }
+
+    }
+}
